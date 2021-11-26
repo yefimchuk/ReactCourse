0e11fc40fb9f4ceee80e08a811d5b738c0a660a0
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-11-26 09:00:00 +0300

    learned components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
 import './App.css';
 
 function App() {
   return (
-  <div className="App-header">
-Hello world
+  <div className="App">
+      <Header/>
+      <div className="App-header">Hello world</div>
 
   </div>
   );
 }
+let Header = () => {
+  return (
+      <div className="Header">
 
+        <div>Home</div>
+        <div>About us</div>
+        <div>Contact</div>
+
+      </div>
+)
+}
 export default App;

52aa99dbf79e60128bfa0262ca42687b2ac70727
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-12-02 21:01:27 +0300

    learned grid module

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import Header from "./Header";
 function App() {
   return (
   <div className="App">
-      <Header/>
-      <div className="App-header">Hello world</div>
+
+      <div className="App-header">
+          <div className="box box1">
+              <div className="nested">a</div>
+              <div className="nested">b</div>
+              <div className="nested">c</div>
+          </div>
+          <div className="box box2">Two</div>
+          <div className="box box3">Three</div>
+          <div className="box box4">Four</div>
+          <div className="box box5">Five</div>
+      </div>
 
   </div>
   );

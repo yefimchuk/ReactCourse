7fe4d2390865fd5f33ad265d48a0cb48f255c1e9
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-12-17 20:00:00 +0300

    added avatar with props, fix exeptions fix personal data

---
+++ src/Components/Content/Profile/ProfileInfo/PersonalData.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import s from "../ProfileInfo/PersonalData.module.css"
+
+let PersonalData = (props) => {
+    return (
+        <div className={s.account}>
+            <div>
+                <img className={s.accountAvatar}
+                     src={props.avatar}/>
+            </div>
+            <div className={s.personalData}>
+
+                <div className={s.text1}>{props.name}</div>
+                <div className={s.text}>Date of birth: {props.born}</div>
+                <div className={s.text}>City: {props.city}</div>
+                <div className={s.text}>Education: {props.education}</div>
+                <div className={s.text}>Web Site: <a href={props.site}> GitHub</a></div>
+            </div>
+        </div>
+
+    )
+}
+export default PersonalData

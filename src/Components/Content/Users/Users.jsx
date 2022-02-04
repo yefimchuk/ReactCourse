80132ab8287e17965ba343a29212c3e5b0930cda
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-02-04 22:20:20 +0200

    added button follow/unfollow by users reduceradded button follow/unfollow by users reducer

--- src/Components/Content/Users/Users.jsx
+++ src/Components/Content/Users/Users.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import s from "./Users.module.css"
+
 let Users = (props) => {
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <div className={s.User}>
-                    <div >
-                        <img />
-                        <div>follow</div>
+                    <div>
+                        <img className={s.avatar} src={u.avatar}/>
+                        <div>{
+                            u.followed ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> :
+                                <button  onClick={() => {props.follow(u.id)}}>Follow</button>
+                        }</div>
                     </div>
                     <div className={s.UserInfo}>
                         <div>
--- src/Components/Redux/userspage-reducer.ts
+++ src/Components/Redux/userspage-reducer.ts
@@ -8,7 +8,7 @@ let initialState = {
             id: 1,
             nickname: "Illya",
             status: "pipapupipu",
-            avatar: "sting",
+            avatar: "https://www.meme-arsenal.com/memes/91e83b00c27bf4d5bb849a6ac2b81fe5.jpg",
             city: "Belogorodka",
             country: "Ukraine",
             followed: true
@@ -17,31 +17,32 @@ let initialState = {
             id: 2,
             nickname: "Arthur",
             status: "i'm love all Nastya in the world",
-            avatar: "sting",
+            avatar: "https://images.aif.by/007/318/b53425339c1eab74cda53465696a4aaf.jpg",
             city: "Bila cerkva",
             country: "sting",
             followed: true
         },
         {
-            id: 2,
+            id: 3,
             nickname: "Luba",
             status: "u are all bitches",
-            avatar: "sting",
+            avatar: "https://www.biography.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU3MTAwNTIzNDc5MTE1MDY1/lana-del-rey-attends-the-60th-annual-grammy-awards-at-madison-square-garden-on-january-28-2018-in-new-york-city-photo-by-dimitrios-kambouris_getty-images-for-naras-square.jpg",
             city: "Kryshchul",
             country: "Florida",
-            followed: true
+            followed: false
         },
     ]
 }
 export const UsersReducers = (state = initialState, action: any) => {
 
     switch (action.type) {
-
         case follow: {
             return {
+
                 ...state,
                 users: state.users.map((u: { id: number }) => {
                     if (u.id === action.id) {
+
                         return {...u, followed: true}
                     }
                     return u

59459c23486d5d0162b83de24534b5cdc5b42434
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-02-13 23:20:30 +0200

    added worker loading gif

--- src/Components/Content/Users/UsersContainer.jsx
+++ src/Components/Content/Users/UsersContainer.jsx
@@ -1,42 +1,53 @@
 import {connect} from "react-redux";
 import {
     followActionCreator,
+    IsLoginActionCreator,
     SetCurrentPageActionCreator,
     SetNewTotalCountActionCreator,
     SetUsersActionCreator,
     unfollowActionCreator,
 } from "../../Redux/userspage-reducer";
 
-
+import loading from "../../../img/35771931234507.564a1d2403b3a.gif"
 import React from "react";
 import * as axios from "axios";
 import Users from "./Usersc";
-
+import s from "./../Users/Users.module.css"
 class UsersAPIContainer extends React.Component {
 
     componentDidMount() {
 
+        this.props.isLogin(true)
+
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=173&count=${this.props.pageSize}`).then(a => {
+            this.props.isLogin(false)
+
             this.props.setUsers(a.data.items)
             this.props.setNewTotalCount(a.data.totalCount)
         })
     }
 
     onPageChanged = (currentPage) => {
-
+        this.props.isLogin(true)
         this.props.setCurrentPage(currentPage)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`).then(a => {
+            this.props.isLogin(false)
             this.props.setUsers(a.data.items)
         })
     }
 
     render() {
 
-        return <Users totalUserCount={this.props.totalUserCount} users={this.props.users}
-                      pageSize={this.props.pageSize} currentPage={this.props.currentPage}
-                      onPageChanged={this.onPageChanged}
-                        follow={this.props.follow}
-                        unfollow={this.props.unfollow}/>
+        return <>
+
+            {this.props.IsLogin ? <div className={s.loading}><img  className={s.loading1} src={loading}/></div> : null}
+            {!this.props.IsLogin ? <Users totalUserCount={this.props.totalUserCount} users={this.props.users}
+                   pageSize={this.props.pageSize} currentPage={this.props.currentPage}
+                   onPageChanged={this.onPageChanged}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}/>
+                : null}
+        </>
     }
 }
 
@@ -64,8 +75,13 @@ let mapDispatchToProps = (dispatch) => {
         setNewTotalCount: (total) => {
             let action = SetNewTotalCountActionCreator(total);
             dispatch(action)
-        }
+        },
+        isLogin: (isLogin) => {
+            let action = IsLoginActionCreator(isLogin)
 
+            dispatch(action)
+
+        }
     }
 }
 
@@ -75,7 +91,8 @@ let mapStateToProps = (state) => {
         users: state.userPage.users,
         pageSize: state.userPage.pageSize,
         totalUserCount: state.userPage.totalUserCount,
-        currentPage: state.userPage.currentPage
+        currentPage: state.userPage.currentPage,
+        IsLogin: state.userPage.isLogin
     }
 }
 
--- src/Components/Redux/userspage-reducer.ts
+++ src/Components/Redux/userspage-reducer.ts
@@ -3,45 +3,13 @@ const unFollow = "UNFOLLOW";
 const setUsers = "SET-USERS";
 const setCurrentPage = "SET-CURRENT-PAGE";
 const setTotalUserCount = "SET-TOTAL-USER-COUNT"
+const IsLogin = "IS-LOGIN"
 let initialState = {
-    users: [
-        /*{
-            id: 1,
-            name: "Illya",
-            status: "pipapupipu",
-            photos:{
-                small: "https://www.meme-arsenal.com/memes/91e83b00c27bf4d5bb849a6ac2b81fe5.jpg"
-            } ,
-            city: "Belogorodka",
-            country: "Ukraine",
-            followed: true
-        },
-        {
-            id: 2,
-            name: "Arthur",
-            status: "i'm love all Nastya in the world",
-            photos:{
-                small: "https://images.aif.by/007/318/b53425339c1eab74cda53465696a4aaf.jpg"
-            } ,
-            city: "Bila cerkva",
-            country: "Ukraine",
-            followed: true
-        },
-        {
-            id: 3,
-            name: "Luba",
-            status: "u are all bitches",
-            photos: {
-                small: "https://mfm.ua/wp-content/uploads/2015/12/lana_del_Rey2.jpg",
-            },
-            city: "Florida",
-            country: "America",
-            followed: false
-        },*/
-    ],
+    users: [],
     pageSize: 100,
     totalUserCount: 0,
-    currentPage: 0
+    currentPage: 0,
+    isLogin: true
 }
 export const UsersReducers = (state = initialState, action: any) => {
 
@@ -94,6 +62,14 @@ export const UsersReducers = (state = initialState, action: any) => {
 
             }
         }
+        case IsLogin: {
+
+            return {
+
+                ...state, isLogin: action.isLogin
+            }
+
+        }
         default:
             return state
     }
@@ -117,5 +93,7 @@ export const SetNewTotalCountActionCreator = (totalUserCount: number) => {
     return {type: setTotalUserCount, totalUserCount: totalUserCount}
 }
 
-
+export const IsLoginActionCreator = (isLogin: boolean) => {
+    return {type: IsLogin, isLogin: isLogin}
+}
 

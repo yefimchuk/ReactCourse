e77284540fcc90fb5bbbf3ae751e749032ca226d
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-04-21 19:19:47 +0300

    do layout for edit profile

--- src/Components/Content/Profile/ProfileInfo/PersonalData.tsx
+++ src/Components/Content/Profile/ProfileInfo/PersonalData.tsx
@@ -20,9 +20,16 @@ let PersonalInformation = ({
 }: PropsT) => {
   return (
     <div className="profile__account">
-      <div>
-        <img className="profile__account-avatar" src={avatar != null ? avatar : photo} />
-      </div>
+
+      <label htmlFor="upload-photo" className="profile__status-text">
+          <input type="file" name="photo" id="upload-photo" />
+          <img
+            className="profile__account-avatar"
+            src={avatar != null ? avatar : photo}
+          />
+        </label>
+
+
       <div className="profile__personal-data-form">
         <div className="profile__personal-data-name">
           Name: <div className="profile__personal-data_text-name">{name}</div>
@@ -30,21 +37,30 @@ let PersonalInformation = ({
         <div className="profile__personal-data_text">Job: {job}</div>
         <div className="profile__personal-data_text">
           GitHub:
-          <a href={`https://` + git} className="profile__personal-data_text-name">
+          <a
+            href={`https://` + git}
+            className="profile__personal-data_text-name"
+          >
             {" "}
             {git}
           </a>
         </div>
         <div className="profile__personal-data_text">
           Instagram:
-          <a href={`https://` + youTube} className="profile__personal-data_text-name">
+          <a
+            href={`https://` + youTube}
+            className="profile__personal-data_text-name"
+          >
             {" "}
             {youTube}
           </a>
         </div>
         <div className="profile__personal-data_text">
           YouTube:
-          <a href={`https://` + instagram} className="profile__personal-data_text-name">
+          <a
+            href={`https://` + instagram}
+            className="profile__personal-data_text-name"
+          >
             {" "}
             {instagram}
           </a>
--- src/Components/Content/Profile/ProfileInfo/Status/Status.tsx
+++ src/Components/Content/Profile/ProfileInfo/Status/Status.tsx
@@ -1,88 +1,155 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import "./status.scss";
 import {useFormik} from "formik";
 import {UpdateStatusThunk} from "../../../../../BLL/ProfilePage/profilePage";
 import {useDispatch} from "react-redux";
+import {GithubOutlined, InstagramOutlined, UserOutlined, YoutubeOutlined} from "@ant-design/icons";
 
-let Edit = ({status}: { status: any }) => {
-  let dispatch = useDispatch();
-  let [editMode, setEditMode] = useState(false);
-  let [UseStatus, setNewStatus] = useState("");
-  let activatedEditMode = () => {
-    setNewStatus(status);
-    setEditMode(true);
-  };
-  let deactivatedEditMode = () => {
-    setEditMode(false);
-    dispatch(UpdateStatusThunk(formik.values.status));
-  };
+let Edit = ({
+                name,
+                status,
+                job,
+                gitHub,
+                instagram,
+                youTube,
+            }: any) => {
+    let dispatch = useDispatch();
 
-  const formik = useFormik({
-    initialValues: {
-      status: status,
-    },
-    onSubmit: (values) => {
-    },
-  });
-  useEffect(() => {
-    setNewStatus(formik.values.status);
-  }, [formik.values.status]);
+    let [editMode, setEditMode] = useState(false);
+    // states for edit
+    let [UseStatus, setNewStatus] = useState("");
+    let upload = (e: any) => {
 
-  return (
-      <div className="profile__status">
-        {editMode ? (
-            <div className="profile__edit_activated">
-              <span className="profile__status-text">Name</span>
-              <input
-                  className="profile__status-input"
+        console.log("hello")
+      return   e.target.files[0];
+    }
+    let activatedEditMode = () => {
+        setEditMode(true);
+    };
+    const save = () => {
+        setEditMode(false);
+        dispatch(UpdateStatusThunk(formik.values.status));
+    };
+    let cancel = () => {
+        setEditMode(false);
+        dispatch(UpdateStatusThunk(formik.values.status));
+    };
 
-              />
-              <span className="profile__status-text">status</span>
-              <input
-                  className="profile__status-input"
-                  id="status"
-                  name="status"
-                  onChange={formik.handleChange}
-                  value={UseStatus}
-                  autoFocus={true}
 
-              />
-              <span className="profile__status-text">job</span>
-              <input
-                  className="profile__status-input"
+    const formik = useFormik({
+        initialValues: {
+            name: name,
+            status: status,
+            job: job,
+            gitHub: gitHub,
+            instagram: instagram,
+            youTube: youTube,
+        },
+        onSubmit: (values) => {
+        },
+    });
 
-              />
-              <span className="profile__status-text">gitHub</span>
-              <input
-                  className="profile__status-input"
 
-              />
-              <span className="profile__status-text">instagram</span>
-              <input
-                  className="profile__status-input"
+    return (
+        <div className="profile__edit">
+            {editMode ? (
+                <div className="profile__edit_activated">
 
-              />
-              <span className="profile__status-text">youTube</span>
+                    <input type="file" name="photo" id="upload-photo" onChange={upload}/>
 
-            <button onClick={deactivatedEditMode}>save</button>
-            </div>
-        ) : (
-            /* lookingForAJob: required(boolean)
-                    lookingForAJobDescription: required(string)
-                    fullName: required(string)
-                    contacts: required(object)
-                    github: required(string)
-                    vk: required(string)
-                    facebook: required(string)
-                    instagram: required(string)
-                    twitter: required(string)
-                    website: required(string)
-                    youtube: required(string)
-                    mainLink: required(string)*/
-            <div className="profile__status-text" onClick={activatedEditMode}>
-              Edit mode
-            </div>
-        )}
+                    <span className="profile__status-text">Name</span>
+                    <input
+                        className="profile__edit-input"
+                        placeholder="Name"
+                        id="names"
+                        name="names"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+
+                    />
+
+
+                    <span className="profile__status-text">Status</span>
+                    <textarea
+                        className="profile__edit-input"
+                        placeholder="Status"
+                        id="status"
+                        name="status"
+                        onChange={formik.handleChange}
+                        value={formik.values.status}
+
+                    />
+
+                    <div className="profile__edit_link" >
+                        <UserOutlined style={{fontSize: '25px', color: '#bbbaba'}}/>
+                        <input
+                            className="profile__edit-input"
+                            placeholder="Job"
+                            id="job"
+                            name="job"
+                            onChange={formik.handleChange}
+                            value={formik.values.job}
+
+                        />
+                    </div>
+                    <div className="profile__edit_link">
+                        <GithubOutlined style={{fontSize: '25px', color: '#bbbaba'}}/>
+                        <input
+                            className="profile__edit-input"
+                            placeholder="GitHub"
+                            id="gitHub"
+                            name="gitHub"
+                            onChange={formik.handleChange}
+                            value={formik.values.gitHub}
+                        />
+                    </div>
+                    <div className="profile__edit_link">
+                        <InstagramOutlined style={{fontSize: '25px', color: '#bbbaba'}}/>
+                        <input
+                            className="profile__edit-input"
+                            placeholder="Instagram"
+                            id="instagram"
+                            name="instagram"
+                            onChange={formik.handleChange}
+                            value={formik.values.instagram}
+                        />
+                    </div>
+                    <div className="profile__edit_link">
+                        <YoutubeOutlined style={{fontSize: '25px', color: '#bbbaba'}}/>
+                        <input
+                            className="profile__edit-input"
+                            placeholder="YouTube"
+                            id="youTube"
+                            name="youTube"
+                            onChange={formik.handleChange}
+                            value={formik.values.youTube}
+
+                        />
+                    </div>
+                    <div className="profile__edit_link">
+                        <div className="profile__edit__buttons">
+                            <button className="profile__edit__buttons_save" onClick={save}>Save</button>
+                            <button className="profile__edit__buttons_cancel" onClick={cancel}>Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                /* lookingForAJob: required(boolean)
+                            lookingForAJobDescription: required(string)
+                            fullName: required(string)
+                            contacts: required(object)
+                            github: required(string)
+                            vk: required(string)
+                            facebook: required(string)
+                            instagram: required(string)
+                            twitter: required(string)
+                            website: required(string)
+                            youtube: required(string)
+                            mainLink: required(string)*/
+                <div className="profile__edit-button" onClick={activatedEditMode}>
+                    Edit mode
+                </div>
+            )}
       </div>
   );
 };
